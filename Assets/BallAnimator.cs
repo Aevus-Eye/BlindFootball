e0770913f4d740a6fe75e4f97a019ed2f948e788
mischaschmid <93661618+mischaschmid@users.noreply.github.com>
2024-04-02 14:40:10 +0200

    add visible animation

---
+++ Assets/BallAnimator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAnimator : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        // Überprüfen, ob das kollidierende Objekt ein Spieler ist
+        if (collision.collider.tag == "Player") {
+            // Trigger 'hit' auslösen, um die hitAnimation zu starten
+            animator.SetTrigger("hit");
+            print("hit");
+        }
+    }   
+}
